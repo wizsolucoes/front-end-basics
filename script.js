f22df3970200352e6deade22b981818bb077062f
unknown <thaisgo.viana@gmail.com>
2019-12-16 23:37:21 -0300

    Criação dos arquivos principais e organização da estrutura

---
+++ script.js
@@ -0,0 +1,7 @@
+function gerar(){
+    var num = document.getElementById('num').value;
+    console.log('O número foi: ', num)
+
+    var result = num * 10
+    console.log('O resultado é: ', result)
+}
