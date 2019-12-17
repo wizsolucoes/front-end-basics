3c49eda25d43ffe3e3974ebe82d17a0957df1731
unknown <thaisgo.viana@gmail.com>
2019-12-17 01:17:47 -0300

    Função de mostrar número na tela

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+numeros = []
 function gerar(){
-    var num = document.getElementById('num').value;
-    console.log('O número foi: ', num)
+    var max = document.getElementById('num').value;
+    var result = Math.floor(Math.random() * 100 + 1);
 
-    var result = num * 10
-    console.log('O resultado é: ', result)
+    if(this.numeros == null || this.numeros.length == 0){ 
+        for (i=0; i < max; i++){
+            var sort = Math.floor(Math.random() * 100 + 1)
+            this.numeros.push(sort)
+        } 
+    } else{
+        this.numeros = []
+        for (i=0; i < max; i++){
+            var sort = Math.floor(Math.random() * 100 + 1)
+            this.numeros.push(sort)
+        }
+    }
+
+    document.getElementById("random").innerHTML = this.numeros;
+    console.log('O número foi: ', max)
+    console.log('O array é: ', numeros)
+    console.log('--------------')
+
 }
