fe83d9300249c863873ba635d36b291cf075811e
unknown <thaisgo.viana@gmail.com>
2019-12-21 00:17:01 -0300

    Novos cálculos

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 numeros = []
+crescente = []
+decrescente = []
+par = []
+ímpar = []
+
 function gerar(){
     var max = document.getElementById('num').value;
     var result = Math.floor(Math.random() * 100 + 1);
@@ -16,9 +21,26 @@ function gerar(){
         }
     }
 
-    document.getElementById("random").innerHTML = this.numeros;
+    document.getElementById("random_text").innerHTML = this.numeros.toString().replace(/,/g, ', ');
     console.log('O número foi: ', max)
     console.log('O array é: ', numeros)
     console.log('--------------')
 
+    this.crescente = this.numeros
+    this.crescente.sort(sortCrescente);
+    console.log('Ordenado', this.crescente)
+    // document.getElementById("crescent").innerHTML = this.crescente.toString().replace(/,/g, ', ');
+
+    this.decrescente = this.numeros
+    this.decrescente.sort(sortDescrescente);
+    console.log('Desordenado', this.decrescente)
+    // document.getElementById("decrescent").innerHTML = this.decrescente.toString().replace(/,/g, ', ');
+}
+
+function sortCrescente(a, b){
+    return (a - b)
+}
+
+function sortDescrescente(a, b){
+    return (b - a)
 }
